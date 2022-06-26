e7f439ace53fc482ef0fea781ba903326d8dc73d
cong <cunbinnak>
2022-06-26 16:12:28 +0700

    update code

--- src/main/java/managerhotel/DAO/IloginDao.java
+++
@@ -1,7 +0,0 @@
-package managerhotel.DAO;
-
-import managerhotel.model.Users;
-
-public interface IloginDao {
-	public Users getUser(String userName);
-}
--- src/main/java/managerhotel/Service/ILoginService.java
+++
@@ -1,8 +0,0 @@
-package managerhotel.Service;
-
-import managerhotel.model.Users;
-
-public interface ILoginService {
-
-	public Users getUser(String userName);
-}
