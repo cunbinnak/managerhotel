bf10ec2832f2a8ae8c7fbdf23d0e2341fea00faf
Vernya <tllongvl3@gmail.com>
2022-06-28 21:53:57 +0700

    Search room

---
+++ src/main/java/com/manager/service/RoomService.java
@@ -0,0 +1,16 @@
+package com.manager.service;
+
+import com.manager.dto.SearchRoomRequest;
+import com.manager.entity.Room;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface RoomService {
+
+    void create(Room room) throws SQLException;
+
+    List<Room> findAllRoom(SearchRoomRequest rq) throws SQLException;
+
+    Room getRoomDetail(String id) throws SQLException;
+}
